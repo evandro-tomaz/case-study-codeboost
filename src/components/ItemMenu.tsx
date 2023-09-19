9c1bd0ed56b5f401779886b23223f6142a22ccc3
Evandro Tomaz <tmzplaydev@gmail.com>
2023-09-19 00:09:38 -0300

    First commit

---
+++ src/components/ItemMenu.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image"
+import Arrow from '@/assets/arrow-down.svg'
+
+type Props = {
+    name: string
+}
+
+export function ItemMenu({ name }: Props) {
+    return (
+        <div>
+            <button className="flex items-center gap-3">
+                <span className="text-white font-bold">{name}</span>
+                <Image
+                    src={Arrow}
+                    alt="Arrow Dropdown"
+                />
+            </button>
+        </div>
+    )
+}
